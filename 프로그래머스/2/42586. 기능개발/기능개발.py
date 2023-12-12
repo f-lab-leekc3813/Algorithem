09b6613a4f676997c5c4897fa55b68dde74e7126
leekc3813 <93689662+leekc3813@users.noreply.github.com>
2023-12-12 21:34:46 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -0,0 +1,21 @@
+def solution(progresses, speeds):
+    answer = []
+    days = 0
+    cnt = 0 #완료된 기능
+    while len(progresses) > 0:
+        if(progresses[0] + days*speeds[0]) >= 100:
+            progresses.pop(0)
+            speeds.pop(0)
+            # 완료된 기능 수
+            cnt += 1
+        else:
+            if cnt > 0:
+                answer.append(cnt)
+                cnt = 0
+            else:
+                days += 1
+    answer.append(cnt)
+    return answer
+                
+    
+    
